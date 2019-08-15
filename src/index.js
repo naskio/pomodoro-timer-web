851459c80d274ef4f1e1740f734391ef839268f9
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-08-15 23:21:05 +0100

    app config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './app/app';
 import * as serviceWorker from './serviceWorker';
 
