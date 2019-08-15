06989a0aab8e9e86cb672589189ff3e7e735d81a
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-08-16 00:48:07 +0100

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const DEFAULT_ROUNDS = 1;
+export const DEFAULT_ROUNDS = 4;
 export const DEFAULT_TASK = '';
-export const DEFAULT_ROUND_TIME = 5;
-export const DEFAULT_BREAK_TIME = 3;
+export const DEFAULT_ROUND_TIME = 1500;
+export const DEFAULT_BREAK_TIME = 300;
